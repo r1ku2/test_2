adbd554cdd7ab4296a5ba8431059144848fbe4f4
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-01-26 16:16:36 +0900

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def main():
+    # Streamlit code
+
+    st.title('Simple Streamlit sample application')
+    st.markdown('** Hello World!!')
+#   
+if __name__ == '__main__':
+    main()
